3bf3dbefe2bfd8d264af8a6f702f21494216b753
jwhiteys <justin.skweres@yahoo.com>
2012-03-14 11:02:35 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beeknr::Application.config.secret_token = '9b3240a12df2923d4770395d62288817b09156df8961fe3d7d0b2eef0e0ac47e129e57d90664a2171882cdc71127e4c56c1f7e266240056004e87f2026c3f593'
